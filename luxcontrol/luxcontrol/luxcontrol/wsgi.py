789c0068b4cc1cbcecc43e2b1f648485e8a726f3
chaeynz <chaeynz4msg@gmail.com>
2025-09-03 19:03:52 +0200

    chore: renamed luxbuster django to luxcontrol and moved the application into its own subdirectory from the root

--- luxcontrol/luxcontrol/luxcontrol/wsgi.py
+++ luxcontrol/luxcontrol/luxcontrol/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for luxbuster project.
+WSGI config for luxcontrol project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "luxbuster.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'luxcontrol.settings')
 
 application = get_wsgi_application()
