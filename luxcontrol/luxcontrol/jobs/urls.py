1ca201a1f7f13ba94b2c470d45d170f6ce6d9479
chaeynz <chaeynz4msg@gmail.com>
2025-09-06 23:40:40 +0000

    WIP messing around

--- luxcontrol/luxcontrol/jobs/urls.py
+++ luxcontrol/luxcontrol/jobs/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import JobsView
 
 urlpatterns = [
-    path('/', JobsView.as_view(), name='idk'),
+    path('', JobsView.as_view(), name='jobs'),
 ]
--- luxcontrol/luxcontrol/jobs/views.py
+++ luxcontrol/luxcontrol/jobs/views.py
@@ -1,19 +1,20 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework import status
 from .serializers import JobSerializer
 from .models import Result
 
 
 class JobsView(APIView):
+    permission_classes = [AllowAny]
+
     # permission_classes = [IsAuthenticated]
 
     def get(self, request):
-        return (request.data)
+        return Response(request.data)
 
     def post(self, request):
-        return (request.data)
         serializer = JobSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save(worker=request.user)
